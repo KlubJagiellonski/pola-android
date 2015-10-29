53731cc1816aab51980c4b47b8023c1a61298083
Michal Tajchert <mtajchert@gmail.com>
2015-10-29 15:52:50 +0100

    Horizontal list of images with last item being button to add photo.

--- app/src/main/java/pl/pola_app/helpers/Utils.java
+++ app/src/main/java/pl/pola_app/helpers/Utils.java
@@ -1,10 +1,19 @@
 package pl.pola_app.helpers;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.provider.Settings;
 
 public class Utils {
     public static String getDeviceId(Context context) {
         return Settings.Secure.getString(context.getContentResolver(), Settings.Secure.ANDROID_ID);
+    }
+
+    public static int dpToPx(int dp) {
+        return (int) (dp * Resources.getSystem().getDisplayMetrics().density);
+    }
+
+    public static int pxToDp(int px) {
+        return (int) (px / Resources.getSystem().getDisplayMetrics().density);
     }
 }
