4896caa5556aca6f83d10d461a227c070af196fd
karol <karkaminski@gmail.com>
2021-11-04 22:04:26 +0100

    wywalone duplikaty w build.gradle i string w AcivityWebView

--- app/src/main/java/pl/pola_app/ui/activity/ActivityWebView.java
+++ app/src/main/java/pl/pola_app/ui/activity/ActivityWebView.java
@@ -9,6 +9,7 @@ import androidx.webkit.WebSettingsCompat;
 import androidx.webkit.WebViewFeature;
 
 import pl.pola_app.databinding.ActivityAboutBinding;
+import pl.pola_app.helpers.Utils;
 
 public class ActivityWebView extends AppCompatActivity {
     @Override
@@ -38,8 +39,7 @@ public class ActivityWebView extends AppCompatActivity {
         if (getIntent() != null && getIntent().getExtras() != null && getIntent().getExtras().containsKey("url")) {
             binding.webView.loadUrl(getIntent().getExtras().getString("url"));
         } else {
-            binding.webView.loadUrl("https://www.pola-app.pl/m/about");
-//            binding.webView.loadUrl(Utils.URL_POLA_ABOUT);
+            binding.webView.loadUrl(Utils.URL_POLA_ABOUT);
         }
         binding.webView.setWebChromeClient(new WebChromeClient());
     }
